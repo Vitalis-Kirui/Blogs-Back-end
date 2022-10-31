ac48a67625837b6723523dd5781241bb770d32ab
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-10-31 17:47:01 +0300

    responding to get requests for home and about paths

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ app.use(express.static('assets'));
 
 // Listening to requests
 app.listen(3000);
+
+// Listening to home page
+app.get('/', (req, res) =>{
+    res.sendFile('./views/home.html', {root : __dirname})
+});
+
+// Listening to about page
+app.get('/about-us', (req, res) =>{
+    res.sendFile('./views/about.html', {root : __dirname});
+});
