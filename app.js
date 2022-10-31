cf41f09d1bd2e359fc92abcc16d3c8fb4e91ec4b
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-10-31 17:49:33 +0300

    creating redirect functions

--- app.js
+++ app.js
@@ -9,11 +9,20 @@ app.use(express.static('assets'));
 app.listen(3000);
 
 // Listening to home page
-app.get('/', (req, res) =>{
+app.get('/home', (req, res) =>{
     res.sendFile('./views/home.html', {root : __dirname})
 });
 
 // Listening to about page
 app.get('/about-us', (req, res) =>{
     res.sendFile('./views/about.html', {root : __dirname});
+});
+
+// Redirection
+app.get('/about', (req, res) =>{
+    res.redirect('/about-us');
+});
+
+app.get('/', (req, res) =>{
+    res.redirect('/home');
 });
