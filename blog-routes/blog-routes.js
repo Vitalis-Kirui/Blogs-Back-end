ff8cc0f059688ef4f0f1105cb8dc9dc9a79d7b86
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-10-31 20:13:02 +0300

    creating blog routes in a different file

---
+++ blog-routes/blog-routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+ const router = express.Router();
+
+ const blogControllers = require('../controllers/blog-controllers');
+
+//  Fetch all blogs
+router.get('/', blogControllers.all_blogs);
+
+// Creating a new blog
+router.get('/create-blog', blogControllers.create_blog);
+
+// Getting a single blog
+router.get('/single-blog', blogControllers.single_blog);
